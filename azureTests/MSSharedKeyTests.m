3f5a8a7d80376bb24910a74bf5e38402836659b4
Andre Rodrigues <andrerod@microsoft.com>
2013-09-02 15:03:45 +0200

    Add signing test

--- azureTests/MSSharedKeyTests.m
+++ azureTests/MSSharedKeyTests.m
@@ -39,4 +39,19 @@
     
 }
 
+- (void)testSignRequest
+{
+    /*
+    var webResource = WebResource.get('container');
+    webResource.withQueryOption(QueryStringConstants.RESTYPE, 'container');
+    webResource.withHeader(HeaderConstants.CONTENT_TYPE, '');
+    webResource.withHeader(HeaderConstants.STORAGE_VERSION_HEADER, HeaderConstants.TARGET_STORAGE_VERSION);
+    webResource.withHeader(HeaderConstants.DATE_HEADER, 'Fri, 23 Sep 2011 01:37:34 GMT');
+    
+    sharedkey.signRequest(webResource, function () {
+        assert.equal(webResource.headers[HeaderConstants.AUTHORIZATION], 'SharedKey devstoreaccount1:Y5R86+6XE5MH602SIyjeTwlJuQjbawv20PT4kb/F/04=');
+
+        */
+}
+
 @end
